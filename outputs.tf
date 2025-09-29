a5731272f78044a387e75799961fd4ea3b6f8a8c
shaik-aisha <58479027+shaik-aisha@users.noreply.github.com>
2025-09-29 18:36:07 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,24 @@
+# S3 Bucket
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.my_bucket.bucket
+  description = "The name of the S3 bucket"
+}
+
+# RDS Endpoint
+output "rds_endpoint" {
+  value       = aws_db_instance.my_rds.endpoint
+  description = "RDS instance endpoint"
+}
+
+# EFS ID
+output "efs_id" {
+  value       = aws_efs_file_system.my_efs.id
+  description = "EFS file system ID"
+}
+
+# ALB DNS
+output "alb_dns_name" {
+  value       = aws_lb.my_alb.dns_name
+  description = "ALB DNS name"
+}
 
