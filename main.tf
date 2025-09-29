8bcc34fa97ff5b4f3d1563ab18fb28fcd67f5115
shaik-aisha <58479027+shaik-aisha@users.noreply.github.com>
2025-09-29 18:33:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
+# S3
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = var.s3_bucket_name
+}
+
+# RDS
+resource "aws_db_instance" "my_rds" {
+  allocated_storage = var.db_allocated_storage
+  engine            = "mysql"
+  instance_class    = var.db_instance_class
+  name              = var.db_name
+  username          = var.db_username
+  password          = var.db_password
+}
+
+# EFS
+resource "aws_efs_file_system" "my_efs" {
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+}
+
+# ALB
+resource "aws_lb" "my_alb" {
+  name               = var.alb_name
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = var.subnets
+}
 
