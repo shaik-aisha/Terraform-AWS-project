d8131f1723b28f1ff3f1fd03b082e62bce58e02a
shaik-aisha <58479027+shaik-aisha@users.noreply.github.com>
2025-09-29 19:12:27 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,74 +1,34 @@
-variable "aws_region" {
+variable "region" {
   type    = string
   default = "ap-south-1"
 }
 
-# EC2
-variable "ec2_instance_type" {
-  type    = string
-  default = "t2.micro"
-}
-variable "ec2_name" {
-  type    = string
-  default = "My-first-terraform-EC2"
-}
-
-# S3
-variable "s3_bucket_name" {
-  type    = string
-  default = "My-first-terraform-S3"
-}
-
-# RDS
-variable "db_name" {
-  type    = string
-  default = "My-first-terraform-RDS"
-}
-variable "db_allocated_storage" {
-  type    = number
-  default = 20
-}
-variable "db_engine" {
-  type    = string
-  default = "mysql"
-}
-variable "db_engine_version" {
-  type    = string
-  default = "8.0"
-}
-variable "db_instance_class" {
-  type    = string
-  default = "db.t2.micro"
-}
-variable "db_username" {
-  type    = string
-  default = "admin"
-}
-variable "db_password" {
-  type    = string
-  default = "Password123!"
-}
-
-# EFS
-variable "efs_name" {
-  type    = string
-  default = "My-first-terraform-EFS"
-}
-variable "subnet_id" {
-  type = string
-}
-variable "security_group_id" {
+variable "vpc_id" {
   type = string
 }
 
-# ALB
-variable "alb_name" {
-  type    = string
-  default = "My-first-terraform-ALB"
-}
 variable "alb_subnets" {
   type = list(string)
 }
-variable "vpc_id" {
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "s3_bucket_name" {
+  type = string
+}
+
+variable "efs_name" {
   type = string
 }
