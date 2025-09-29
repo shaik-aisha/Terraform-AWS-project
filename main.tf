dda4ad782e48544dad68cec10dc31b98ad4ebb29
shaik-aisha <58479027+shaik-aisha@users.noreply.github.com>
2025-09-29 20:08:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
-# Provider
 provider "aws" {
   region = var.aws_region
 }
 
-# -----------------------------
+###########################
 # S3 Bucket
-# -----------------------------
+###########################
 resource "aws_s3_bucket" "my_bucket" {
   bucket = var.s3_bucket_name
 }
@@ -15,9 +14,9 @@ resource "aws_s3_bucket_acl" "my_bucket_acl" {
   acl    = "private"
 }
 
-# -----------------------------
+###########################
 # EC2 Instance
-# -----------------------------
+###########################
 resource "aws_instance" "my_ec2" {
   ami           = var.ec2_ami
   instance_type = var.ec2_instance_type
@@ -28,58 +27,59 @@ resource "aws_instance" "my_ec2" {
   }
 }
 
-# -----------------------------
-# RDS Instance
-# -----------------------------
-resource "aws_db_instance" "my_rds" {
-  identifier             = var.db_identifier
-  engine                 = var.db_engine
-  engine_version         = var.db_engine_version
-  instance_class         = var.db_instance_class
-  allocated_storage      = var.db_allocated_storage
-  username               = var.db_username
-  password               = var.db_password
-  db_subnet_group_name   = var.db_subnet_group
-  skip_final_snapshot    = true
-}
+###########################
+# RDS Database
+###########################
+resource "aws_db_subnet_group" "rds_subnets" {
+  name       = "${var.db_identifier}-subnet-group"
+  subnet_ids = var.alb_subnets  # or create a separate variable for DB subnets
 
-# -----------------------------
-# EFS
-# -----------------------------
-resource "aws_efs_file_system" "efs" {
-  creation_token = var.efs_name
   tags = {
-    Name = var.efs_name
+    Name = "${var.db_identifier}-subnet-group"
   }
 }
 
+resource "aws_db_instance" "my_rds" {
+  identifier             = var.db_identifier
+  allocated_storage      = var.db_allocated_storage
+  engine                 = var.db_engine
+  engine_version         = var.db_engine_version
+  instance_class         = var.db_instance_class
+  username               = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnets.name
+  skip_final_snapshot    = true
+}
+
+###########################
+# EFS Filesystem
+###########################
+resource "aws_efs_file_system" "efs" {
+  creation_token = var.efs_name
+}
+
 resource "aws_efs_mount_target" "efs_mount" {
-  for_each        = toset(var.efs_subnet_ids)
-  file_system_id  = aws_efs_file_system.efs.id
-  subnet_id       = each.value
+  for_each       = toset(var.efs_subnet_ids)
+  file_system_id = aws_efs_file_system.efs.id
+  subnet_id      = each.value
   security_groups = [var.security_group_id]
 }
 
-# -----------------------------
-# ALB (Application Load Balancer)
-# -----------------------------
-data "aws_subnet" "first_alb_subnet" {
-  id = var.alb_subnets[0]
-}
-
+###########################
+# Application Load Balancer
+###########################
 resource "aws_lb" "my_alb" {
   name               = var.alb_name
-  internal           = false
   load_balancer_type = "application"
-  security_groups    = [var.security_group_id]
   subnets            = var.alb_subnets
+  security_groups    = [var.security_group_id]
 }
 
 resource "aws_lb_target_group" "tg" {
   name     = "${var.alb_name}-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = data.aws_subnet.first_alb_subnet.vpc_id
+  vpc_id   = var.vpc_id
 }
 
 resource "aws_lb_listener" "listener" {
@@ -91,4 +91,27 @@ resource "aws_lb_listener" "listener" {
     type             = "forward"
     target_group_arn = aws_lb_target_group.tg.arn
   }
+}
+
+###########################
+# Outputs
+###########################
+output "ec2_public_ip" {
+  value = aws_instance.my_ec2.public_ip
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.my_rds.endpoint
+}
+
+output "efs_id" {
+  value = aws_efs_file_system.efs.id
+}
+
+output "alb_dns_name" {
+  value = aws_lb.my_alb.dns_name
 }
