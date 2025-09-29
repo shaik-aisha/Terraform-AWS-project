1c33783e3ab133fc75a6fb0112fb8b366f780499
shaik-aisha <58479027+shaik-aisha@users.noreply.github.com>
2025-09-29 19:31:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
-# AWS region
 variable "aws_region" {
-  description = "AWS region to deploy resources"
+  description = "AWS region"
   type        = string
   default     = "ap-south-1"
 }
 
-# EC2
 variable "ec2_instance_type" {
   description = "EC2 instance type"
   type        = string
@@ -13,27 +11,31 @@ variable "ec2_instance_type" {
 }
 
 variable "ec2_name" {
-  description = "EC2 instance name tag"
+  description = "EC2 instance Name tag"
   type        = string
   default     = "my-ec2"
 }
 
-# S3
 variable "s3_bucket_name" {
   description = "S3 bucket name"
   type        = string
-  default     = "my-terraform-s3-bucket"
+  default     = "my-terraform-bucket-123"
 }
 
-# RDS
-variable "db_allocated_storage" {
-  description = "RDS allocated storage (GB)"
-  type        = number
-  default     = 20
+variable "db_name" {
+  description = "RDS database name"
+  type        = string
+  default     = "mydatabase"
+}
+
+variable "db_identifier" {
+  description = "RDS instance identifier"
+  type        = string
+  default     = "my-rds-instance"
 }
 
 variable "db_engine" {
-  description = "RDS database engine"
+  description = "RDS engine"
   type        = string
   default     = "mysql"
 }
@@ -47,43 +49,47 @@ variable "db_engine_version" {
 variable "db_instance_class" {
   description = "RDS instance class"
   type        = string
-  default     = "db.t2.micro"
+  default     = "db.t3.micro"
 }
 
-variable "db_name" {
-  description = "Database name"
+variable "db_allocated_storage" {
+  description = "RDS allocated storage in GB"
+  type        = number
+  default     = 20
+}
+
+variable "db_username" {
+  description = "RDS master username"
   type        = string
-  default     = "mydb"
+  default     = "admin"
 }
 
-# EFS
+variable "db_password" {
+  description = "RDS master password"
+  type        = string
+  default     = "Admin123!"
+}
+
 variable "efs_name" {
-  description = "EFS name"
+  description = "EFS filesystem name"
   type        = string
   default     = "my-efs"
 }
 
-variable "subnet_id" {
-  description = "Subnet ID for EFS mount"
-  type        = string
-  default     = ""
-}
-
-variable "security_group_id" {
-  description = "Security group ID for EC2, EFS, and ALB"
-  type        = string
-  default     = ""
-}
-
-# ALB
 variable "alb_name" {
-  description = "ALB name"
+  description = "Application Load Balancer name"
   type        = string
   default     = "my-alb"
 }
 
 variable "alb_subnets" {
-  description = "List of subnets for ALB"
+  description = "List of subnet IDs for ALB"
   type        = list(string)
   default     = []
+}
+
+variable "security_group_id" {
+  description = "Security group ID for EC2, ALB, EFS"
+  type        = string
+  default     = ""
 }
