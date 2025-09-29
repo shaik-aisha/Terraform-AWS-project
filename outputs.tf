bce4690a31d5d5313e61ff0348b1d5550220e8b7
shaik-aisha <58479027+shaik-aisha@users.noreply.github.com>
2025-09-29 18:59:08 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,23 @@
-# S3 Bucket
+output "ec2_id" {
+  value = aws_instance.my_ec2.id
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.my_ec2.public_ip
+}
+
 output "s3_bucket_name" {
-  value       = aws_s3_bucket.my_bucket.bucket
-  description = "The name of the S3 bucket"
+  value = aws_s3_bucket.my_bucket.id
 }
 
-# RDS Endpoint
 output "rds_endpoint" {
-  value       = aws_db_instance.my_rds.endpoint
-  description = "RDS instance endpoint"
+  value = aws_db_instance.my_rds.endpoint
 }
 
-# EFS ID
 output "efs_id" {
-  value       = aws_efs_file_system.my_efs.id
-  description = "EFS file system ID"
+  value = aws_efs_file_system.my_efs.id
 }
 
-# ALB DNS
 output "alb_dns_name" {
-  value       = aws_lb.my_alb.dns_name
-  description = "ALB DNS name"
+  value = aws_lb.my_alb.dns_name
 }
-
